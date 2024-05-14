a5045b006ff0ae5805e9be018fde93b4590f4666
zoubair ghandi <zoubair.ghandi@gmail.com>
2024-05-14 02:50:32 +0200

    first day 1  commit

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React , { useState } from 'react'
+import Todo from './Todo'
 
 const TodoWrapper = () => {
+
+    const [values, setValue] = useState([]);
+    function handleChange(e) {
+        setValue(e.target.value);
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('You clicked submit.');
+      }
+
   return (
-    <div>TodoWrapper</div>
+    <div>
+        <label> Add task : <input name="myInput" value={values} onChange={handleChange} /><button onClick={handleSubmit}>Add New task</button></label>
+        <hr/>
+        <Todo/>
+    </div>
   )
 }
 
